a8154e8fb898a149cd4486e35501b792d079a960
张川 <storezhang@gmail.com>
2023-03-31 12:27:56 +0800

    Update starter.go

--- starter.go
+++ starter.go
@@ -4,10 +4,8 @@ import (
 	"github.com/pangum/pangu"
 )
 
-// Starter 入口封装，由于语言特性，最好是直接继承的方式，通过显示使用主入口来执行初始化流程
-type Starter struct {
-	// TODO 从第三方要接入的主入口继承
-}
+// Starter 方便调用引用时
+type Starter = TODO
 
 func newAgent(config *pangu.Config) (starter *Starter, err error) {
 	_config := new(panguConfig)
